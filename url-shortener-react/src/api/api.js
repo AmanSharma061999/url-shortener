34cc4e7d621bdc9bae1b2b6f5d1e54b5a4f9aa51
AmanSharma061999 <123433492+AmanSharma061999@users.noreply.github.com>
2026-02-18 15:10:12 -0700

    samp

--- url-shortener-react/src/api/api.js
+++ url-shortener-react/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://ec2-3-86-58-131.compute-1.amazonaws.com:8081";
+  "http://ec2-3-86-58-131.compute-1.amazonaws.com:8081/api";
 
 const api = axios.create({
   baseURL,
